7a38b7a792e8d53ee8318fb114fa8a7b31fed3f2
MoonFather <moonfather@homemail.com>
2025-09-12 12:05:24 +0200

    starting neoforge

--- README.md
+++
@@ -1,5 +0,0 @@
-# Vegan mod
-Small Minecraft mod.
-
-
-License:  [MLL](https://github.com/moonfather1/playablepeaceful/blob/main/LICENSE.md) due to one CC licensed texture.
--- src/main/java/moonfather/vegan_mod/VeganMod.java
+++ src/main/java/moonfather/vegan_mod/VeganMod.java
@@ -1,97 +1,93 @@
 package moonfather.vegan_mod;
 
-import moonfather.vegan_mod.changes.RecipeManagerMain;
-import moonfather.vegan_mod.items.ArmorUncraftingRecipe;
-import moonfather.vegan_mod.items.FullBottleItem;
-import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditionType;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
-import net.minecraft.core.Registry;
-import net.minecraft.core.component.DataComponentType;
+import com.mojang.logging.LogUtils;
+
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Unit;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
-public class VeganMod implements ModInitializer
-{
-	public static final String MOD_ID = "vegan_mod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(VeganMod.MODID)
+public class VeganMod {
+    public static final String MODID = "vegan_mod";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
-	@Override
-	public void onInitialize()
-	{
-		Items.initialize();
-		Other.initialize();
-		// https://wiki.fabricmc.net/tutorial:items#creating_items_in_1212
 
-		// remove 4 cutting.
-		// logo
-		// ink2 dead
-		// options
-		// simple mode
-		// create crushing  --- 1.20.1
-		// 1.21.8  models, recipes different,  resin
-		///////////////////////
-		Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, ResourceLocation.fromNamespaceAndPath(MOD_ID, "armor_cutting"), ArmorUncraftingRecipe.getSerializerForRegistration());
-		//////////
-		ResourceConditionType<?> conditionTypeForOptionalRecipes = ResourceConditionType.create(ResourceLocation.fromNamespaceAndPath(MOD_ID, "optional"), OptionalRecipeCondition.CODEC);
-		OptionalRecipeCondition.setType(conditionTypeForOptionalRecipes);
-		ResourceConditions.register(conditionTypeForOptionalRecipes);
-		//////////
-		ResourceConditionType<?> conditionTypeForOptionalRecipes2 = ResourceConditionType.create(ResourceLocation.fromNamespaceAndPath(MOD_ID, "tag_not_empty"), TagNotEmptyRecipeCondition.CODEC);
-		TagNotEmptyRecipeCondition.setType(conditionTypeForOptionalRecipes2);
-		ResourceConditions.register(conditionTypeForOptionalRecipes2);
-	}
 
-	///////////////////////////////////////
 
-	public static class Items
-	{
-		public static final Item HARDENED_FABRIC = new Item(new Item.Properties());
-		public static final Item PLANT_OIL = new FullBottleItem();
-		public static final Item PLANT_INK = new FullBottleItem();
+    public VeganMod(IEventBus modEventBus, ModContainer modContainer)
+    {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
 
-		//////////////////////////////////////////////////////
+        Items.init(modEventBus);
 
-		public static void initialize()
-		{
-			Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "hardened_fabric"), HARDENED_FABRIC);
-			Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "plant_ink"), PLANT_INK);
-			Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "plant_oil"), PLANT_OIL);
+        NeoForge.EVENT_BUS.register(this);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
 
-			ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS).register(Items::addToCreativeTabs);
+    private void commonSetup(FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+        if (Config.LOG_DIRT_BLOCK.getAsBoolean())
+        {
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+        }
+        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
+        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
+    }
 
-			ServerLifecycleEvents.SYNC_DATA_PACK_CONTENTS.register(RecipeManagerMain::beforeSync);
-		}
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("HELLO from server starting");
+    }
 
-		private static void addToCreativeTabs(FabricItemGroupEntries entries)
-		{
-			entries.accept(HARDENED_FABRIC);
-			entries.accept(PLANT_INK);
-			entries.accept(PLANT_OIL);
-		}
+    public class Items
+    {
+        private Items() { }
+        private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+        public static void init(IEventBus modEventBus)
+        {
+            ITEMS.register(modEventBus);
+            modEventBus.addListener(Items::addCreative);
+        }
+        public static final DeferredItem<Item> PLANT_OIL = ITEMS.registerSimpleItem("plant_oil", new Item.Properties());
+        public static final DeferredItem<Item> PLANT_INK = ITEMS.registerSimpleItem("plant_ink", new Item.Properties());
+        public static final DeferredItem<Item> HARDENED_FABRIC = ITEMS.registerSimpleItem("hardened_fabric", new Item.Properties());
+        private static void addCreative(BuildCreativeModeTabContentsEvent event)
+        {
+            if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+            {
+                event.accept(PLANT_INK);
+                event.accept(PLANT_OIL);
+                event.accept(HARDENED_FABRIC);
+            }
+        }
+    }
 
-		private Items() {}
-	}
-
-	/////////////////////////////
-
-	public static class Other
-	{
-		public static final DataComponentType<Unit> VEGAN_MARKER = DataComponentType.<Unit>builder().persistent(Unit.CODEC).build();
-
-		public static void initialize()
-		{
-			Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, ResourceLocation.fromNamespaceAndPath(MOD_ID, "vegan_made"), VEGAN_MARKER);
-		}
-		private Other() {}
-	}
 }
